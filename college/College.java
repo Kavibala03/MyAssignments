0814adff4eacc451f7fb2f2872fe0cd945f1e6be
Kavibala03 <145035988+Kavibala03@users.noreply.github.com>
2023-10-10 23:00:21 +0530

    Add files via upload

---
+++ college/College.java
@@ -0,0 +1,20 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Indira Gandhi");
+	}
+	public void collegeCode() {
+		System.out.println("041");
+	}
+	public void collegeRank() {
+		System.out.println("5");
+	}
+	public static void main(String[] args) {
+		College info=new College();
+		info.collegeName();
+		info.collegeCode();
+		info.collegeRank();
+	}
+
+}
