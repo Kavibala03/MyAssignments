78b5a732fc65f3c3698c9103eca12bc76caa32d2
Kavibala03 <145035988+Kavibala03@users.noreply.github.com>
2023-10-15 22:54:35 +0530

    Add files via upload

---
+++ FindSecondLargest.java
@@ -0,0 +1,31 @@
+package week5.day3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class FindSecondLargest{
+	public static void main(String[] args) {
+		int[] num= {3,2,11,4,6,7,2,3,3,6,7};
+		Set<Integer> number=new TreeSet<Integer>();
+		Arrays.sort(num);
+		for (int i = 0; i < num.length; i++) {
+			number.add(num[i]);
+		}
+		List<Integer> numlist=new ArrayList<Integer>(number);
+		if(numlist.size()>=2) {
+			int sec=numlist.get(numlist.size()-2);
+			System.out.println("Sec:" +sec);
+		}
+		else
+			System.out.println("No second largest element");
+	}
+	
+}
+
+
+
+
+
